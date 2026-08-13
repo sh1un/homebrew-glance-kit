d2a92505d5d628f98ada708a1c1a08f10caab657
sh1un <a0923183408@gmail.com>
2026-08-14 02:07:39 +0800

    Add galance-kit 0.4.0 cask

---
+++ Casks/galance-kit.rb
@@ -0,0 +1,24 @@
+cask "galance-kit" do
+  version "0.4.0"
+  sha256 "551e842492852d411fc333415b1bf3bbad9d60bca64bee30c31da50f7a7beb15"
+
+  url "https://github.com/sh1un/homebrew-glance-kit/releases/download/v#{version}/GalanceKit-#{version}.zip"
+  name "GalanceKit"
+  desc "Grafana panels and top traffic metrics in the macOS menu bar"
+  homepage "https://github.com/sh1un/homebrew-glance-kit"
+
+  depends_on macos: ">= :sonoma"
+
+  app "GlanceKit.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/dev.shiun.glancekit",
+    "~/Library/Application Scripts/dev.shiun.glancekit.widget",
+    "~/Library/Application Scripts/group.dev.shiun.glancekit",
+    "~/Library/Containers/dev.shiun.glancekit",
+    "~/Library/Containers/dev.shiun.glancekit.widget",
+    "~/Library/Group Containers/group.dev.shiun.glancekit",
+    "~/Library/Preferences/dev.shiun.glancekit.plist",
+    "~/Library/WebKit/dev.shiun.glancekit",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,17 @@
+# Homebrew tap for galance-kit
+
+Install `galance-kit` with:
+
+```sh
+brew tap sh1un/glance-kit
+brew install --cask galance-kit
+```
+
+Upgrade to the latest published version with:
+
+```sh
+brew update
+brew upgrade --cask galance-kit
+```
+
+The Cask downloads the Developer ID-signed and Apple-notarized release from this repository's [Releases](https://github.com/sh1un/homebrew-glance-kit/releases). The application source remains in the private `galance-kit` repository.
